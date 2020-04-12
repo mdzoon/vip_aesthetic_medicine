03fa307bed9cd5effc5e2e9d5903f869b66351cd
Your Name <you@gmail.com>
2020-04-12 20:18:02 +0100

    added Cookies Policy and Privacy Policy pages

---
+++ pages/cookies-policy.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <h1>Cookies Policy</h1>
+        <p>Like almost all websites, our website uses cookies, to help provide you with the best experience we can. Cookies are small text files that are placed on your computer or mobile phone when you browse websites.</p>
+        <p>This Cookies Policy explains the cookies we use and the purpose for which we use them for our website located at <a href="/">www.vipaestheticmedicine.co.uk</a> and its associated domains (collectively the “Site”).</p>
+        <h3>What are Cookies?</h3>
+        <p>A cookie is a small text file that a website saves on your computer or mobile device when you visit the site. It enables the website to remember your actions and preferences (such as login, language, font size and other display preferences) over a period, so you don’t have to keep re-entering them whenever you come back to the site or browse from one page to another.</p>
+        <h3>Analytics Cookies</h3>
+        <p>We may collect information about your computer, including your IP address, operating system and browser type, for system administration and to create reports. This information is statistical data about our users’ browsing actions and patterns, and does not identify you as an individual.</p>
+        <p>Cookies contain information transferred to your computer’s hard drive. These cookies are used to store information, such as the time that the current visit occurred, whether the visitor has been to the website before and what site referred the visitor to the web page.</p>
+        <p>We only use ‘session’ cookies on our site to make it easier for visitors to engage with the site.</p>
+        <h3>Third-Party Functions</h3>
+        <p>Our site, like most websites, includes functionality provided by the following third parties which could use cookies. For example Google Maps.</p>
+        <p>Disabling these cookies will likely break the functions offered by these third parties.</p>
+        <h3>How to Control Cookies?</h3>
+        <p>You can control and delete cookies as you wish – for details, see <a href="https://www.aboutcookies.org" target="_blank">www.aboutcookies.org</a>. You can remove all cookies that are already on your computer, and you can set most browsers to prevent them from being placed. If you do this, however, you may have to adjust some preferences manually every time you visit a website, and therefore some services and functionalities may not work.</p>
+        <p>Please do not hesitate to contact us regarding any matter relating to this Cookie Policy by sending us an email.</p>
+    </div>
+</template>
---
+++ pages/privacy-policy.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <h1>Privacy Policy</h1>
+        <h3>What Personal Data We Collect And Why We Collect It</h3>
+        <p>We may collect and process the following data about you:</p>
+        <ul>
+            <li>Information that you provide by filling in forms on the Site. This includes information provided at the time of registering to use the Site as a customer or providing content.</li>
+            <li>Records of any correspondence between you and us.</li>
+            <li>Details of transactions you carry out through the Site and of the fulfilment of your orders.</li>
+            <li>Details of your visits to the Site and the resources that you access.</li>
+        </ul>
+        <h3>Where We Store Your Information</h3>
+        <p>The data that we collect from you may be transferred to, and stored at, a destination outside the European Economic Area (“EEA”). It may also be processed by staff operating outside the EEA who work for us or for one of our suppliers. Such staff may be engaged in, among other things, the fulfilment of your order, the processing of your payment details and the provision of support services. By submitting your personal data, you agree to this transfer, storing or processing. We will take all steps reasonably necessary to ensure that your data is treated securely and in accordance with this Privacy Policy.</p>
+        <p>Unfortunately, the transmission of information via the Internet is not completely secure. We will do our best to protect your personal data, but cannot guarantee the security of data transmitted to our site; any transmission is at your own risk. Once we have received your information, we will use strict procedures and security features to try to prevent unauthorised access.</p>
+        <h3>Uses Made of the Information</h3>
+        <p>We use information held about you in the following ways:</p>
+        <ul>
+            <li>To ensure that content from the Site is presented in the most effective manner for you and for your computer.</li>
+            <li>To provide you with information or services that you request from us.</li>
+            <li>To carry out our obligations arising from the Terms of Use, this Privacy Policy, the Intellectual Property Policy, the Cookies Policy, the Fees Policy and the Returns Policy.</li>
+            <li>To notify you about changes to our service.</li>
+        </ul>
+        <p>If you have registered on the Site, we may occasionally update you on our latest products, news, special offers and events via e-mail. All registered users have the option to opt-out of receiving marketing communications from us. You can do this by clicking on the Unsubscribe link in any email communications that we might send you.</p>
+        <h3>Disclosure of Your Information</h3>
+        <p>We may disclose your personal information to third parties:</p>
+            <ul>
+                <li>In the event that InimitableMe sells or buys any business or assets;</li>
+                <li>If InimitableMe or substantially all of its assets are acquired by a third party, in which case personal data which we hold about our customers may be one of the transferred assets; or,</li>
+                <li>If we are under a duty to disclose or share your personal data in order to comply with any legal obligation, or in order to enforce or apply our Terms of Use; or to protect the rights, property, or safety of InimitableMe, our customers, or others. This includes exchanging information with other companies and organisations for the purposes of fraud protection and credit risk reduction.</li>
+            </ul>
+            <p>We may disclose aggregate, anonymised statistics about the number of visitors to the Site or number and value of purchases made as required by current or prospective investors and partners. Such statistics may include information about visits to pages on the site, the duration of individual page views, paths taken by visitors through the site, data on visitors’ screen settings and other general information. InimitableMe uses and stores this type of information, as with that obtained from other cookies used on the site, to help it improve the services to its users.</p>
+        <h3>Your Rights</h3>
+        <p>We will not sell your name, address, e-mail address, credit or debit card information or personal information to any third party without your permission.</p>
+        <p>If you wish to verify the details you have submitted to InimitableMe you may do so by contacting us via the e-mail address or address given below. Our security procedures mean that we may request proof of identity before we reveal information. This proof of identity will take the form of your e-mail address and password submitted upon registration. You must therefore keep this information safe as you will be responsible for any action which we take in response to a request from someone using your e-mail and password. We would strongly recommend that you do not use the browser’s password memory function as that would permit other people using your terminal to access your personal information.</p>
+        <p>You have the right to access the information that InimitableMe holds about you and your right of access can be exercised in accordance with the Act. Any access request may be subject to a fee of £10 to meet our costs in providing you with details of the information we hold about you.</p>
+        <h3>Your Use of Information</h3>
+        <p>As part of a transaction, you may obtain personal information, including email address and shipping information, from another InimitableMe user. Without obtaining prior permission from that user, you may only use that personal information for the purpose of completing that transaction or for InimitableMe related communications. You may not use that personal information for unsolicited commercial messages. Without limiting the foregoing, without obtaining prior permission from that user, you may not add any InimitableMe user to any email or physical mailing list.</p>
+        <h3>Third Party Sites</h3>
+        <p>The Site may contain links to and from the websites of our partners, advertisers and other third parties. If you follow a link to any of these websites, please note that they have their own privacy policies and that we do not accept any responsibility or liability for these policies. Please check these policies before you submit any personal data to these websites.</p>
+    </div>
+</template>
