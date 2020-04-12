9de1a2970da32e4def4b864eba8b0a68752d84f3
Your Name <you@gmail.com>
2020-04-12 16:08:33 +0100

    installed SASS, congfigured nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,11 +5,11 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'VIP Aesthetic Medicine' || process.env.npm_package_name,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'Aesthetic medicine, health treatment and dermatology services in Bolton' || process.env.npm_package_description }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -41,6 +41,11 @@ export default {
     // Doc: https://bootstrap-vue.js.org
     'bootstrap-vue/nuxt',
   ],
+  styleResources: {
+    scss: [
+      '~assets/scss/_variables.scss' // use underscore "_" & also file extension ".scss"
+    ]
+  },
   /*
   ** Build configuration
   */
@@ -50,5 +55,11 @@ export default {
     */
     extend (config, ctx) {
     }
+  },
+  /*
+  ** Generate configuration
+  */
+  generate: {
+    fallback: true
   }
 }
