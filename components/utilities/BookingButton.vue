adf29b2c8aacbb8ea4a7fd27362202f4a1092ddd
Michael Dyczkowski <michaeldyczkowski@gmail.com>
2020-06-02 23:27:41 +0100

    added BookingButton component

---
+++ components/utilities/BookingButton.vue
@@ -0,0 +1,7 @@
+<template>
+    <button class="btn btn-danger my-2 my-sm-0" type="submit">
+        <i class="material-icons-outlined">event_available</i>
+        <div class="gutter"></div>
+        Book Your Consultation
+    </button>
+</template>
